dd0a3a54cc8ec340ef08d108f9812afb2ab5d22a
superhzf <superhzf@hotmail.com>
2020-01-18 10:50:33 -0800

    unique paths

--- DP/Unique Paths/solution.py
+++ DP/Unique Paths/solution.py
@@ -16,3 +16,9 @@ class Solution:
             for j in range(n):
                 count[i][j] = count[i-1][j] + count[i][j-1]
         return count[m-1][n-1]
+
+# we can split this problem into several basic problems,
+# if we want to reach table[i][j], we can reach from table[i-1][j] or table[i][j-1],
+# so, as long as we know table[i-1][j] and table[i][j-1], problem solved.
+# To me, the key is that we have to realize that the # of ways to reach table[0][j] == 1
+# and the number of ways to reach table[i][0] == 1
