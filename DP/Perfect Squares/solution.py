6cb172b1c42e8ac4fa30478c3f50ee820e661ad8
superhzf <superhzf@hotmail.com>
2020-01-21 19:32:05 -0800

    perfect squares

--- DP/Perfect Squares/solution.py
+++ DP/Perfect Squares/solution.py
@@ -17,7 +17,13 @@ class Solution:
                     dp[i] = min(dp[i], 1 + dp[i-temp])
         return dp[n]
 
+class Solution:
+    def numSquares(self, n: int) -> int:
+        DP = [0]
+        for i in range(1,n+1): DP.append(1 + min(DP[i-j*j] for j in range(int(i**.5),0,-1)))
+        return DP[n]
 
+# dp.append(i) means at most we have i ways (1+1+1+...)
 # dp[i] shows the least number of perfect square numbers sum to dp[i]
 # dp[i-temp] means that 假如我们要求12，然后temp = 9，那么还剩３，我们只需要知道dp[3]再
 # 加上9代表的这个值即可
