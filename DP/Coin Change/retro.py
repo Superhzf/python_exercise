8e5ad33140bf79ac473d00e908cc7bcabed033f8
Zefu Hu <zefuhu@Zefus-MacBook-Pro.local>
2020-01-24 13:54:11 -0800

    add retro

---
+++ DP/Coin Change/retro.py
@@ -0,0 +1,9 @@
+def coinChange(coins, amount):
+    dp = [float('inf')]*amount
+    dp[0] = 0
+
+    for coin in coins:
+        for i in (coin,amount+1):
+            dp[i] = min(dp[i],dp[amount-i]+1)
+
+    return dp[amount] if dp[amount] != float('inf') else -1
--- DP/Coin Change/solution.py
+++ DP/Coin Change/solution.py
@@ -3,10 +3,11 @@ class Solution:
         dp = [float('inf')] * (amount + 1)
         dp[0] = 0
 
-        for coin in coins:
+        for coin in coins: # Under the condition that this coin is used
             for x in range(coin, amount + 1):
                 dp[x] = min(dp[x], dp[x - coin] + 1)
         return dp[amount] if dp[amount] != float('inf') else -1
 
 # This is similar to Perfect Squares problem
-# dp[i] means how many coins needed to sum to i
+# dp[i] means how many coins needed to sum to i udner the condition that
+# a certain coin is used
