643b1c226cb3ae1813c765d58f05340aa929f0bc
superhzf <superhzf@hotmail.com>
2020-02-19 15:01:56 -0800

    type: feat
    Add range sum of BST problem

---
+++ Recursion/Range Sum of BST/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        def dfs(node):
+            if node:
+                if node.val>=L and node.val<=R:
+                    self.ans+=node.val
+                if node.val > L:
+                    dfs(node.left)
+                if node.val < R:
+                    dfs(node.right)
+
+        self.ans = 0
+        dfs(root)
+        return self.ans
+
+# Have to study DFS and binary search tree
