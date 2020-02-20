ab03b80309e8148a5a7074bc0982dbaa2fa83ff5
superhzf <superhzf@hotmail.com>
2020-02-19 17:25:53 -0800

    type: style
    Add more comments to implementation of BFS

--- Tree/BFS.py
+++ Tree/BFS.py
@@ -1,3 +1,21 @@
+class Node:
+    def __init__(self,x):
+        self.key = x
+        self.left = None
+        self.right = None
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+#    1
+#    /\
+#   2  3
+#  /\
+# 4  5
+
 # BFS:
 def BFS(root):
     queue = []
