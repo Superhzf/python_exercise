ef5ebfc0f8bbd30cfe3e6f549f496777bace0a68
superhzf <superhzf@hotmail.com>
2020-02-20 18:15:50 -0800

    type: feat
    Add iterative style methods for DFS

--- Tree/DFS_BFS_Traverse_Tree.py
+++ Tree/DFS_BFS_Traverse_Tree.py
@@ -27,7 +27,8 @@ def BFS(root):
             queue.append(temp.left)
         if temp.right:
             queue.append(temp.right)
-# DFS: inorder
+
+# DFS: inorder: left, root, right
 def inorder(root):
     res = []
     if root:
@@ -36,7 +37,23 @@ def inorder(root):
         res = res + inorder(root.right)
     return res
 
-# DFS: preorder
+def inorder_iterate(root):
+    current = root
+    stack = []
+    res = []
+    while True:
+        if current is not None:
+            stack.append(current)
+            current = current.left
+        elif stack:
+            current = stack.pop()
+            res.append(current.key)
+            current = current.right
+        else:
+            break
+    return res
+
+# DFS: preorder: root, left, right
 def preorder(root):
     res = []
     if root:
@@ -45,7 +62,19 @@ def preorder(root):
         res = res+ preorder(root.right)
     return res
 
-# DFS: postorder
+def preorder_iterate(root):
+    queue = [root]
+    res = []
+    while queue:
+        current = queue.pop(0)
+        res.append(current.key)
+        if current.left is not None:
+            queue.append(current.left)
+        if current.right is not None:
+            queue.append(current.right)
+    return res
+
+# DFS: postorder: left, right, root
 def postorder(root):
     res = []
     if root:
@@ -53,3 +82,36 @@ def postorder(root):
         res = res+ postorder(root.right)
         res.append(root.key)
     return res
+
+def postorder_iterative(root):
+
+    res = []
+    # Create two stacks
+    s1 = []
+    s2 = []
+
+    # Push root to first stack
+    s1.append(root)
+
+    # Run while first stack is not empty
+    while s1:
+
+        # Pop an item from s1 and
+        # append it to s2
+        node = s1.pop()
+        s2.append(node)
+
+        # Push left and right children of
+        # removed item to s1
+        if node.left:
+            s1.append(node.left)
+        if node.right:
+            s1.append(node.right)
+
+        # Print all elements of second stack
+    while s2:
+        node = s2.pop()
+        res.append(node.key)
+    return res
+
+# BFS vs DFS, when to use what?
