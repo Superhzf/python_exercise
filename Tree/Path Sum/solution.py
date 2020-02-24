95dfbbf3e1af61079854e64c7354cf9fde041d0b
superhzf <superhzf@hotmail.com>
2020-02-23 16:27:54 -0800

    type: feat
    Add problem about finding a path such that adding up all the values 
    along the path equals the given sum.

---
+++ Tree/Path Sum/solution.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if root == None:
+            return False
+        else:
+            if ((sum - root.val) == 0) and (root.left == None) and (root.right == None):
+                return True
+            if (root.left != None):
+                if (self.hasPathSum(root.left,sum-root.val)):
+                    return True
+            if (root.right != None):
+                if (self.hasPathSum(root.right,sum-root.val)):
+                    return True
+            else:
+                return False
+        return False
+# the key here is to use sum-root.val
