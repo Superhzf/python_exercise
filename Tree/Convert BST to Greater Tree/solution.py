4721274e1ea6bf8c96ec8a3f8966219070c0ded9
superhzf <superhzf@hotmail.com>
2020-02-24 15:11:46 -0800

    type: feat
    Add a problem about adding all nodes that are larger tha a node to it

---
+++ Tree/Convert BST to Greater Tree/solution.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def convertBST(self, root):
+        total = 0
+
+        node = root
+        stack = []
+        while stack or node is not None:
+            # push all nodes up to (and including) this subtree's maximum on
+            # the stack.
+            while node is not None:
+                stack.append(node)
+                node = node.right
+
+            node = stack.pop()
+            total += node.val
+            node.val = total
+
+            # all nodes with values between the current and its parent lie in
+            # the left subtree.
+            node = node.left
+
+        return root
+
+# Push the right node to a stack
