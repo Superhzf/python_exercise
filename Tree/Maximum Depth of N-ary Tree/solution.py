7750f55787a534a95edd6b7d9e95e12f05d62134
superhzf <superhzf@hotmail.com>
2020-02-24 16:34:31 -0800

    type: feat
    Add a problem calculating the max depth of an N-arry tree

---
+++ Tree/Maximum Depth of N-ary Tree/solution.py
@@ -0,0 +1,16 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def maxDepth(self, root):
+        if not root: return 0 #base case 1
+        if not root.children: return 1 #base case 2
+        heights = [] #hold all the heights of root's children
+        for node in root.children:
+            heights.append(self.maxDepth(node))
+        return max(heights) + 1
