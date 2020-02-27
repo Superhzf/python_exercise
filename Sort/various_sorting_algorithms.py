eed4818fd609e2f7597c936bd020df10852d8536
superhzf <superhzf@hotmail.com>
2020-02-27 11:03:10 -0800

    type: feat
    Implement selection sort in a recursive style

--- Sort/various_sorting_algorithms.py
+++ Sort/various_sorting_algorithms.py
@@ -81,7 +81,24 @@ def selectionSort(arr):
     for i in range(1,len(arr)):
         key = arr[i]
         j = i-1
+        # find where to insert the new element
         while j>=0 and key<arr[j]:
             arr[j+1] = arr[j]
             j-=1
         arr[j+1]=key
+
+def selectionSortRecursive(arr,n):
+    # base case
+    if n<=1:
+        return
+
+    selectionSortRecursive(arr,n-1)
+
+    last = arr[n-1]
+    j = n-2
+
+    while (j>=0 and arr[j] > last):
+        arr[j+1] = arr[j]
+        j=j-1
+    arr[j+1] = last
+    return arr
