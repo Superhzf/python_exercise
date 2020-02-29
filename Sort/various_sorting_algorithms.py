0ad181ac062f5f43ef51a73ceeb80b30e90e9d98
superhzf <superhzf@hotmail.com>
2020-02-28 20:36:31 -0800

    type: feat
    Implement quick sort

--- Sort/various_sorting_algorithms.py
+++ Sort/various_sorting_algorithms.py
@@ -106,3 +106,19 @@ def insertionSortRecursive(arr,n):
 
 # Quick sort
 # time complexity: O(nlog(n)), extra space: O()
+def partition(arr,low,high):
+    i = low - 1
+    pivot = arr[high]
+    for j in range(low,high):
+        if arr[j]<pivot:
+            i = i + 1
+            arr[i],arr[j] = arr[j],arr[i]# It is like bubble sort
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return i+1
+
+def quickSort(arr,low,high):
+    if low<high:
+        pi = partition(arr,low,high)
+
+        quickSort(arr,low,pi-1)
+        quickSort(arr,pi+1,high)
