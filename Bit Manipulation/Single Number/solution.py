8c03fc4e6f70c2fdf4a0695b05f947b979d2db0e
superhzf <superhzf@hotmail.com>
2020-02-29 15:24:07 -0800

    type: feat
    Add another solution for finding out the single number in a list

--- Bit Manipulation/Single Number/solution.py
+++ Bit Manipulation/Single Number/solution.py
@@ -7,3 +7,10 @@ class Solution:
             except:
                 hash_table[i]=1
         return hash_table.popitem()[0]
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
