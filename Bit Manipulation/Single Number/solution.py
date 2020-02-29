cdd2dd4e1ff4e4a74b040cde97c0d81242af76f8
superhzf <superhzf@hotmail.com>
2020-02-29 15:17:12 -0800

    type: feat
    Find out the single number

---
+++ Bit Manipulation/Single Number/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_table = {}
+        for i in nums:
+            try:
+                hash_table.pop(i)
+            except:
+                hash_table[i]=1
+        return hash_table.popitem()[0]
