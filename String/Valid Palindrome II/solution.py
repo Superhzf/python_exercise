e6f11ac6139de889a2f1d7ae205176d9574b8d98
Superhzf <superhzf@hotmail.com>
2020-03-10 19:20:31 +0800

    type: feat
    Updated code for Valid Palindrome II

--- String/Valid Palindrome II/solution.py
+++ String/Valid Palindrome II/solution.py
@@ -1,12 +1,7 @@
 class Solution:
-    def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        if s == s[::-1]:
-            return True
-        for i in range(len(s)-1):
-            if s[i] != s[-i-1]:
-                return s[i: -i-2] == s[i+1: -i-1][::-1] or s[i+1: -i-1] == s[i+2: len(s)-i][::-1]
-        return False
+    def validPalindrome(self, s: str) -> bool:
+        n = len(s)
+        for i in range(n//2+1):
+            if s[i] != s[~i]:
+                return s[i+1:n-i] == s[i+1:n-i][::-1] or s[i:~i] == s[i:~i][::-1]
+        return True  
