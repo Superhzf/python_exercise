2e56c9e009056cd582926edcf571b8feb7b0deb0
Superhzf <superhzf@hotmail.com>
2020-03-13 07:58:00 +0800

    type: refactor
    Refactor code for Min Cost Climbing Stairs for simplicity

--- Dynamic Programming/Min Cost Climbing Stairs/solution.py
+++ Dynamic Programming/Min Cost Climbing Stairs/solution.py
@@ -1,9 +1,5 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        if len(cost) == 0:
-            return
-        if len(cost) == 1:
-            return cost[0]
         if len(cost) == 2:
             return min(cost[0],cost[1])
 
