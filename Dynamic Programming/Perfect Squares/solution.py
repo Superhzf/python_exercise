d8cf25f91a435711ac8ab4cd1003491fdc44aa35
Superhzf <superhzf@hotmail.com>
2020-03-13 07:03:37 +0800

    type: refactor
    Add more comments and refactor the code for readability

--- Dynamic Programming/Perfect Squares/solution.py
+++ Dynamic Programming/Perfect Squares/solution.py
@@ -7,7 +7,8 @@ class Solution:
         dp = [0,1,2,3]
 
         for i in range(4,n+1):
-            dp.append(i) # at least we need i square numbers to sum up to i
+            dp.append(i) # at least we need i square numbers to sum up to i (all
+                         # of them are 1)
 
             # the reason to iterate from 1 to int(ceil(sqrt(i))) + 1 instead of
             # just using int(sqrt(i)) is that, int(sqrt(i)) is not necessarily
@@ -22,13 +23,14 @@ class Solution:
 
 class Solution:
     def numSquares(self, n: int) -> int:
-        DP = [0]
-        for i in range(1,n+1): DP.append(1 + min(DP[i-j*j] for j in range(int(i**.5),0,-1)))
-        return DP[n]
+        dp = [0]
+        for i in range(1,n+1):
+            dp.append(1 + min([dp[i-j*j] for j in range(int(i**.5),0,-1)]))
+        return dp[n]
 
 
 # dp.append(i) means at most we have i ways (1+1+1+...)
-# dp[i] shows the least number of perfect square numbers sum to dp[i]
+# dp[i] shows the least number of perfect square numbers sum to i
 # dp[i-temp] means that 假如我们要求12，然后temp = 9，那么还剩３，我们只需要知道dp[3]再
 # 加上9代表的这个值即可
-# The import thing is to find out how state transfers
+# The important thing is to find out how state transfers
