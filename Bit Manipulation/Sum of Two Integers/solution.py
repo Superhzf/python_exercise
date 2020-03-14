c74c7425cd399203d402c35afd0843dfb2c4a5a3
Superhzf <superhzf@hotmail.com>
2020-03-15 07:29:43 +0800

    type: docs
    Add more comments about bit manipulation for problem Sum of Two Integers

--- Bit Manipulation/Sum of Two Integers/solution.py
+++ Bit Manipulation/Sum of Two Integers/solution.py
@@ -24,3 +24,14 @@ def getSum(self, a: int, b: int) -> int:
 
 #more explanation about mask
 # https://leetcode.com/problems/sum-of-two-integers/discuss/489210/Read-this-if-you-want-to-learn-about-masks
+
+# Why we need a mask item?
+# The reason is that in some situations (for example,a=-1,b=2), the carry
+# item will never be 0. So, if the carry item is 100, the solution is to use number like 1000
+# then 100&1000 = 0. The simpliest way is to use 0xffffffff because leetcode
+# uses 32bit
+
+# 原码
+# 反码：若正数，则和原码一致；若负数，则符号位不变，原码其余各位取反
+# 补码：若正数，则和原码一致；若负数，则将数字对应的反码加1
+# 0的反码和补码均为0
