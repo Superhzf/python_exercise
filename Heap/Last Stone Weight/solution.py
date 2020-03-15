1e255d01a7ef70c384d91c5dd39dc8e1b9466495
Superhzf <superhzf@hotmail.com>
2020-03-15 08:41:13 +0800

    type: refactor
    Refactor code for Last Stone Weight for speed

--- Heap/Last Stone Weight/solution.py
+++ Heap/Last Stone Weight/solution.py
@@ -6,8 +6,8 @@ class Solution:
             stones[i]*=-1
         heapq.heapify(stones) # O(n)
         while len(stones)>1:
-            a = abs(h.heappop(stones)) # O(log(n))
-            b = abs(h.heappop(stones))
+            a = h.heappop(stones) # O(log(n))
+            b = h.heappop(stones)
             if a!=b:
-                h.heappush(stones, b-a) # O(log(n))
+                h.heappush(stones, a-b) # O(log(n))
         return abs(stones[0]) if stones else 0
