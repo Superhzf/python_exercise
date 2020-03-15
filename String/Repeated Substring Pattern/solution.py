f6352da1825392cf0cbfed8f0951521d527e23ee
Superhzf <superhzf@hotmail.com>
2020-03-15 09:16:18 +0800

    type: docs
    Add more comments to Repeated Substring Pattern

--- String/Repeated Substring Pattern/solution.py
+++ String/Repeated Substring Pattern/solution.py
@@ -3,7 +3,7 @@ class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         n = len(s)
         for i in range(1, n // 2 + 1):
-            if n % i == 0:
+            if n % i == 0: # It is possiblr to be a solution
                 j = n // i
                 if ''.join([s[:i]] * j) == s:
                     return True
