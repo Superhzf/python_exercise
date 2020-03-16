dc971a012afa1153e75dc2ead08d70f1749146c3
Superhzf <superhzf@hotmail.com>
2020-03-16 08:51:25 +0800

    type: refactor
    Refactor code Maxmimum Depth of N-array Tree for readability

--- Tree/Maximum Depth of N-ary Tree/solution.py
+++ Tree/Maximum Depth of N-ary Tree/solution.py
@@ -5,11 +5,12 @@ class Node:
         self.val = val
         self.children = children
 """
-
 class Solution(object):
     def maxDepth(self, root):
-        if not root: return 0 #base case 1
-        if not root.children: return 1 #base case 2
+        if not root:
+            return 0 #base case 1
+        if not root.children:
+            return 1 #base case 2
         heights = [] #hold all the heights of root's children
         for node in root.children:
             heights.append(self.maxDepth(node))
