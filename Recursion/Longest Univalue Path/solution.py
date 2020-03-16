21f8bfec6cdaaa53a7a3fba3e39d516921fe9879
Superhzf <superhzf@hotmail.com>
2020-03-16 11:16:33 +0800

    type: refactor
    Refactor code for Longest Univalue Path for readability

--- Recursion/Longest Univalue Path/solution.py
+++ Recursion/Longest Univalue Path/solution.py
@@ -10,13 +10,15 @@ class Solution(object):
         self.ans = 0
 
         def arrow_length(node):
-            if not node: return 0
+            if node is None:
+                return 0
             left_length = arrow_length(node.left)
             right_length = arrow_length(node.right)
-            left_arrow = right_arrow = 0
-            if node.left and node.left.val == node.val:
+            left_arrow = 0
+            right_arrow = 0
+            if node.left is not None and node.left.val == node.val:
                 left_arrow = left_length + 1
-            if node.right and node.right.val == node.val:
+            if node.right is not None and node.right.val == node.val:
                 right_arrow = right_length + 1
             self.ans = max(self.ans, left_arrow + right_arrow)
             return max(left_arrow, right_arrow)
