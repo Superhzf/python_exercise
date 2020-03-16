b62cd3b7d6c3bd786fa466e6f819d2c6d8faa977
Superhzf <superhzf@hotmail.com>
2020-03-16 09:12:03 +0800

    type: docs
    Add more comments to Convert a Number to Hex

--- Bit Manipulation/Convert a Number to Hexadecimal/solution.py
+++ Bit Manipulation/Convert a Number to Hexadecimal/solution.py
@@ -1,17 +1,18 @@
 class Solution:
     def toHex(self, num: int) -> str:
-        num = num & (2**32-1)
-        if num<10 and num>=0:
-            return str(num)
+        mapping = {0: '0', 0: '0', 1: '1', 2: '2', 3: '3', 4: '4',5:'5',6:'6',
+                   7: '7', 8: '8', 9: '9', 10: 'a', 11: 'b', 12: 'c', 13: 'd',
+                   14: 'e', 15: 'f'}
 
-        hash_dict ={0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',
-                    8:'8',9:'9',10:'a',11:'b',12:'c',13:'d',
-                    14:'e',15:'f'}
-        stack = []
+        if num == 0:
+            return '0'
+        if num<0:
+            num = num & (2**32-1) # The reason for this is that it will convert
+                                  # the negative number to corresponding
+                                  # positive number after removing the sign
+                                  # based on two's complement
+        res = ''
         while num > 0:
-            remainder = num%16
-            stack.insert(0,hash_dict[remainder])
+            res =  mapping[num%16] + res
             num = num//16
-        return ''.join(stack)
-
-# I don't understand what is num = num & (2**32-1)
+        return res
