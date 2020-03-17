7d81499aff2fb8bd5818436c4832b44914bfa151
Superhzf <superhzf@hotmail.com>
2020-03-17 10:26:00 +0800

    type: refactor
    Refactor code for problem Power of Four for the sake of better 
    understanding

--- Bit Manipulation/Power of Four/solution.py
+++ Bit Manipulation/Power of Four/solution.py
@@ -10,10 +10,15 @@ class Solution:
 
         counts1 = 0
         counts0 = 0
-        if num>0:
-            while num!=0:
-                counts1 += num&1
-                if (num|0) != 1:
+        if num <= 0:
+            return False
+
+        while num!=0:
+            counts1 += num&1 # num&1:check whether the last digit is 1 or not
+            if counts1 > 1:
+                return False
+            else:
+                if num != 1:
                     counts0+=1
                 num >>= 1
         return (counts1==1 and counts0 % 2 == 0)
