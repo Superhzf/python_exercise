eae79103ae0fccb1148425c76020b2e427651700
Superhzf <superhzf@hotmail.com>
2020-03-17 11:39:05 +0800

    type: docs
    Add more comments to sieve algoritms to find out primes less than n

--- Math/Sieve of Eratosthenes.py
+++ Math/Sieve of Eratosthenes.py
@@ -14,3 +14,7 @@ class Solution:
                 for i in range(2*p,n,p):
                     prime[i] = False
         return sum(prime)
+
+# The idea is that iterte from 2,3,4,.. all the way to sqrt(n)+1. Let's see it
+# is p, under the condition that p is a prime, then
+# from 2p, 3p, 4p to n will not be prime numbers
