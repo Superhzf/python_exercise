1984d26d6a1101f41aed8bd3c10f66c57a6754bb
Superhzf <superhzf@hotmail.com>
2020-03-17 16:19:26 +0800

    type: refactor
    Refactor code for Count Negative Numbers in a Sorted Matrix

--- Binary Search/Count Negative Numbers in a Sorted Matrix/solution.py
+++ Binary Search/Count Negative Numbers in a Sorted Matrix/solution.py
@@ -1,13 +1,24 @@
+class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        r, c, cnt = 0, n - 1, 0
-        while r < m and c >= 0:
-            if grid[r][c] < 0:
-                cnt += m - r # if grid[r][c] < 0, the grid[r+1,r+2,..m][c] == 0
-                c -= 1
+        total = 0
+
+        if grid[-1][-1] >=0 :
+            return total
+
+        row = len(grid)
+        col = len(grid[0])
+
+        pos_row = 0
+        pos_col = col - 1
+        while pos_row < row and pos_col >= 0:
+            if grid[pos_row][pos_col] < 0:
+                total = total + row - pos_row # if grid[r][c] < 0, the grid[r+1,r+2,..m][c] == 0
+                pos_col -= 1
             else:
-                r += 1
-        return cnt
+                pos_row += 1
+
+        return total
+
 
 # Consider this problem as a "staircase":
 # ++++++
