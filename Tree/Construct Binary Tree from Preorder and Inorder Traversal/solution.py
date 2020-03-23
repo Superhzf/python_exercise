3e0d1e65150c362e8152dc337698652ebb1cface
Superhzf <superhzf@hotmail.com>
2020-03-23 16:56:11 +0800

    type: feat
    Construct a binary tree from preorder and inorder traversal

---
+++ Tree/Construct Binary Tree from Preorder and Inorder Traversal/solution.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        def helper(in_left = 0, in_right = len(inorder)):
+            nonlocal pre_idx
+            # if there is no elements to construct subtrees
+            if in_left == in_right:
+                return None
+
+            # pick up pre_idx element as a root
+            root_val = preorder[pre_idx]
+            root = TreeNode(root_val)
+
+            # root splits inorder list
+            # into left and right subtrees
+            index = idx_map[root_val]
+
+            # recursion
+            pre_idx += 1
+            # build left subtree
+            root.left = helper(in_left, index)
+            # build right subtree
+            root.right = helper(index + 1, in_right)
+            return root
+
+        # start from first preorder element
+        pre_idx = 0
+        # build a hashmap value -> its index
+        idx_map = {val:idx for idx, val in enumerate(inorder)}
+        return helper()
