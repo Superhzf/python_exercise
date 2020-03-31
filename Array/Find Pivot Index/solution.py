4cbcbf411627e7d867fcd54d5046f541b31eeb4d
Superhzf <superhzf@hotmail.com>
2020-03-31 17:39:45 +0800

    type: docs
    Add more comments to problem Find Pivot Index

--- Array/Find Pivot Index/solution.py
+++ Array/Find Pivot Index/solution.py
@@ -7,3 +7,7 @@ class Solution(object):
                 return i
             leftsum += x
         return -1
+
+# Step 1: calculate the total sum and initiate the left sum as 0
+# Step 2: Iterate each element in nums, calculate whether left sum and right sum
+# is equal to each other
