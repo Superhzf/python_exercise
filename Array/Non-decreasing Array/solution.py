e8d3846135793621064e609c21d98ad77179b91b
Superhzf <superhzf@hotmail.com>
2020-04-08 19:19:55 +0800

    type: refactor
    Refactor code for problem Non-decreasing Array

--- Array/Non-decreasing Array/solution.py
+++ Array/Non-decreasing Array/solution.py
@@ -1,24 +1,14 @@
-class Solution(object):
-    def checkPossibility(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        length = len(nums)
-        flag = 0
-        pos = 0
-        for i in range(length - 1):
-        	if nums[i] > nums[i + 1]:
-        		if flag == 0:
-        			flag = 1
-        			pos = i
-        		else:
-        			return False
-        if flag == 0:
-        	return True
-        if length == pos + 2 or pos == 0:
-        	return True
-        # After iteration, we have to check this
-        if nums[pos-1] <= nums[pos+1] or nums[pos] <= nums[pos+2]:
-        	return True
-        return False
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        p = None
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i+1]:
+                if p is not None:
+                    return False
+                p = i
+
+        return (p is None or p == 0 or p == len(nums)-2 or
+                nums[p-1] <= nums[p+1] or nums[p] <= nums[p+2])
+
+# nums[p-1] <= nums[p+1]： [-1,4,2,3]
+# nums[p] <= nums[p+2]： [2,3,3,2,4]
