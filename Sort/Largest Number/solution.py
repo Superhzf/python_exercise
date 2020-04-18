222d00f0399e018aed5a95158f95c5560b2809ef
Superhzf <superhzf@hotmail.com>
2020-04-18 09:42:34 +0800

    type: feat
    Problem: Largest Number

---
+++ Sort/Largest Number/solution.py
@@ -0,0 +1,48 @@
+class Solution:
+    def mergeSort(self,arr):
+        if len(arr)>1:
+            mid = len(arr)//2
+            L = arr[:mid]
+            R = arr[mid:]
+
+            self.mergeSort(L)
+            self.mergeSort(R)
+
+            i = 0
+            j = 0
+            k = 0
+
+            while i < len(L) and j < len(R):
+                LR = str(L[i]) + str(R[j])
+                RL = str(R[j]) + str(L[i])
+                if LR > RL:
+                    arr[k] = L[i]
+                    i += 1
+                else:
+                    arr[k] = R[j]
+                    j += 1
+                k += 1
+
+
+            # Checking if any element was left
+            while i < len(L):
+                arr[k] = L[i]
+                i+=1
+                k+=1
+
+            while j < len(R):
+                arr[k] = R[j]
+                j+=1
+                k+=1
+
+        res = ''.join(map(str,arr))
+        if set(res) == {'0'}:
+            return '0'
+        return res
+
+
+
+    def largestNumber(self, nums: List[int]) -> str:
+        if len(nums) == 1:
+            return ''.join(map(str,nums))
+        return self.mergeSort(nums)
