1fe58d0e046057635c5342cdffa58462d1784e5c
Superhzf <superhzf@hotmail.com>
2020-04-19 09:45:26 +0800

    type: docs
    Add more comments to problem car fleet

--- Sort/Car Fleet/solution.py
+++ Sort/Car Fleet/solution.py
@@ -1,19 +1,19 @@
+
+# The key to this problem is fully understanding the description.
 class Solution:
 
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars = sorted(zip(position, speed))
-        times = [float(target - p) / s for p, s in cars]
-        ans = 0
+        cars = sorted(zip(position,speed))
+        times = [(target-p)/s for p, s in cars]
+        cnt = 0
         while len(times) > 1:
             lead = times.pop()
             # Per definition, a car can never pass another car ahead of it,
             # so, if a car cannot be caught by the one right after it, it will
             # never be caught
-            if lead < times[-1]:
-                ans += 1
+            if lead >= times[-1]:
+                times[-1] = lead
             # If it can be caught, then the fleet will move on in the speed of leed
-            else: times[-1] = lead
-
-        return ans + bool(times) # remaining car is fleet (if it exists)
-
-# The key to this problem is fully understanding the description.
+            else:
+                cnt += 1
+        return cnt + bool(times)
