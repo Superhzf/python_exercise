61793fb0ac5dd10b2f043808584118c519b5b021
Superhzf <superhzf@hotmail.com>
2020-04-26 22:02:55 +0800

    type: refactor
    Refactor problem Maximum Length of a Concatenated String with Unique 
    Chars for simplicity

--- Backtracking/Maximum Length of a Concatenated String with Unique Characters/solution.py
+++ Backtracking/Maximum Length of a Concatenated String with Unique Characters/solution.py
@@ -4,15 +4,16 @@ class Solution:
 
     def maxLength(self, arr: List[str]) -> int:
 
-        def backtrack(curr_list=[],index=0):
+        def backtrack(first=0):
             curr_str = ''.join(curr_list)
             if len(curr_str) == len(set(curr_str)):
-                self.max_len = max(self.max_len,len(curr_str))
+                self.max_len = max(self.max_len, len(curr_str))
 
-            for i in range(index,len(arr)):
-                curr_list.append(arr[i])
-                backtrack(curr_list, i+1)
+            for curr_pos in range(first, len(arr)):
+                curr_list.append(arr[curr_pos])
+                backtrack(curr_pos + 1)
                 curr_list.pop()
 
+        curr_list = []
         backtrack()
         return self.max_len
