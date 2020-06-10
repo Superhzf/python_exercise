f6fbd8f4645ca6cd7f020d87d40f70f47abc0569
Superhzf <superhzf@hotmail.com>
2020-06-10 17:49:11 +0800

    type: refactor
    Refactor the solution for problem Perfect Squares for people to better 
    understand

--- Dynamic Programming/Perfect Squares/solution.py
+++ Dynamic Programming/Perfect Squares/solution.py
@@ -1,41 +1,25 @@
-from math import ceil, sqrt
+from math import sqrt
 class Solution:
     def numSquares(self, n: int) -> int:
-        if n <= 3:
-            return n
 
-        dp = [0,1,2,3]
+        dp = [0] * (n+1)
+        for i in range(1, n+1):
+            dp[i] = i
 
-        for i in range(4,n+1):
-            dp.append(i) # at least we need i square numbers to sum up to i (all
-                         # of them are 1)
-
-            # the reason to iterate from 1 to int(ceil(sqrt(i))) + 1 instead of
-            # just using int(sqrt(i)) is that, int(sqrt(i)) is not necessarily
-            # the best number
-            for x in range(1,int(ceil(sqrt(i))) + 1):
-                temp = x*x
-                if temp>i:
-                    break
-                else:
-                    dp[i] = min(dp[i], 1 + dp[i-temp])
-        return dp[n]
-
-class Solution:
-    def numSquares(self, n: int) -> int:
-        dp = [0]
-        for i in range(1,n+1):
+        for i in range(1, n+1):
             candidate_list = []
-            for j in range(int(sqrt(i)),0,-1):
+            for j in range(int(math.sqrt(i))+1):
                 candidate_list.append(dp[i-j*j])
-            # 1 means j*j
-            dp.append(1+min(candidate_list))
-
+            dp[i] = min(dp[i], 1+min(candidate_list))
         return dp[n]
 
-
-# dp.append(i) means at most we have i ways (1+1+1+...)
-# dp[i] shows the least number of perfect square numbers sum to i
-# dp[i-temp] means that 假如我们要求12，然后temp = 9，那么还剩３，我们只需要知道dp[3]再
-# 加上9代表的这个值即可
-# The important thing is to find out how state transfers
+# we start from enumerating all valid solutions:
+#               12
+#       11      8       3
+#   10, 7, 2   7, 4
+# we can see that 7 has to be calculated two times
+# 1. overlapped sub-problems
+# as described above
+# 2. the optimized sub-structure and state trasnformation:
+# dp[i] shows the least # of perfect squares numbers needed
+# dp[i] = dp[i-j*j] for j in range(sqrt(i)+1)
