001d4ffd36a464054919096e596096a3599df001
Superhzf <superhzf@hotmail.com>
2020-06-11 16:34:03 +0800

    type: feat
    Add the recursion version of the problem Lowest Common Ancestor of a 
    Binary Tree

--- Tree/Lowest Common Ancestor of a Binary Tree/solution.py
+++ Tree/Lowest Common Ancestor of a Binary Tree/solution.py
@@ -35,3 +35,49 @@ class Solution:
 # Step 2: list all ancestors of p based on step 1
 # Step 3: find ancestors of q one by one, if one of them shows up in step 2, then
 # this is what we are looking for.
+
+class Solution:
+
+    def __init__(self):
+        self.ans = None
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        def recurse_tree(current_node):
+
+            # If reached the end of a branch, return False.
+            if current_node is None:
+                return False
+
+            # If the current node is one of p or q
+            mid = current_node == p or current_node == q
+
+            # Left Recursion
+            left = recurse_tree(current_node.left)
+
+            # Right Recursion
+            right = recurse_tree(current_node.right)
+
+
+            # If any two of the three flags left, right or mid become True.
+            if mid + left + right >= 2:
+                self.ans = current_node
+
+            # Return True if either of the three bool values is True.
+            return mid or left or right
+
+        # Traverse the tree
+        recurse_tree(root)
+        return self.ans
+
+# Recursion is an intuitive solution
+# recurse_tree means whether p or q is found in the tree current_node
+# 1. What does the current node should do?
+# Confirm whether the current node is p or q
+# 2. What does the left or right tree do?
+# Find either p or q or both which has not been found
