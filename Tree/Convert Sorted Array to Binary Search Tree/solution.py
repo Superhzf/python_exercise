59f9fbcf29545b3add23b9fbe83ea324aa64049e
Superhzf <superhzf@hotmail.com>
2020-06-12 16:00:50 +0800

    type: feat
    Add another solution to Converted Sorted Array to BST

--- Tree/Convert Sorted Array to Binary Search Tree/solution.py
+++ Tree/Convert Sorted Array to Binary Search Tree/solution.py
@@ -22,5 +22,23 @@ class Solution:
         node.right = self.helper(mid + 1, j)
 
         return node
-        
+
 # The idea is pretty much like a binary search
+
+# The recursive version
+class Solution:
+
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if len(nums) == 0:
+            return
+        size = len(nums)
+        idx = int(size/2)
+        root_val = nums[idx]
+        root = TreeNode(root_val)
+        root.left = self.sortedArrayToBST(nums[:idx])
+        root.right = self.sortedArrayToBST(nums[idx+1:])
+        return root
+
+# What does the current node do?
+# Find the root node and find out which part of nums belong to the left
+# tree and which belongs to the right tree.
