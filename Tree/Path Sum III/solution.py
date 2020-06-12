2897c1bc6c21df07b047f7dca1a363e613b626cb
Superhzf <superhzf@hotmail.com>
2020-06-12 17:23:33 +0800

    type: feat
    Add a solution to Path Sum III

--- Tree/Path Sum III/solution.py
+++ Tree/Path Sum III/solution.py
@@ -28,3 +28,34 @@ class Solution(object):
             if node.right:
                 stack.append((node.right, [x+node.right.val for x in totals]+[node.right.val]))
         return num
+
+class Solution(object):
+    def pathSum(self, root, total):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        self.total = total
+        self.res = 0
+        self.helper(root, True, self.total)
+        return self.res
+
+    def helper(self, root, start, target_sum):
+        if root is None:
+            return
+        if root.val == target_sum:
+            self.res += 1
+
+        self.helper(root.left, False, target_sum-root.val)
+        self.helper(root.right, False, target_sum-root.val)
+
+        if start:
+            self.helper(root.left, True, self.total)
+            self.helper(root.right, True, self.total)
+
+# What does the current node do?
+# Checkout whether the value is equal to the target sum
+# What does the helper function do?
+# Check whether a sub tree can sum up to the target sum (no matter the target
+# sum is the original required total or total - value of the current node)
