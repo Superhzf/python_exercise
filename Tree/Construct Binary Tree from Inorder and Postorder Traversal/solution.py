526995efccedb813f423e664a4cf4c0b2580321f
Superhzf <superhzf@hotmail.com>
2020-06-13 17:24:21 +0800

    type: feat
    Add another solution for the problem Construct Binary Tree from Inorder 
    and Postorder Traversal

--- Tree/Construct Binary Tree from Inorder and Postorder Traversal/solution.py
+++ Tree/Construct Binary Tree from Inorder and Postorder Traversal/solution.py
@@ -27,3 +27,23 @@ class Solution:
 
         inorder_map = {value:idx for idx,value in enumerate(inorder)}
         return helper()
+
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if len(inorder) == 0:
+            return None
+        root_val = postorder[-1]
+        root = TreeNode(root_val)
+
+        for idx, value in enumerate(inorder):
+            if value == root_val:
+                inorder_left = inorder[:idx]
+                inorder_right = inorder[idx+1:]
+
+        postorder_left = postorder[:len(inorder_left)]
+        postorder_right = postorder[len(inorder_left):-1]
+
+        root.left = self.buildTree(inorder_left, postorder_left)
+        root.right = self.buildTree(inorder_right, postorder_right)
+        return root
