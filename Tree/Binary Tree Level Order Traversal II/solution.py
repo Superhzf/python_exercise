fce7d274979367b78b9a80829b5bd28c29caaec5
Superhzf <superhzf@hotmail.com>
2020-06-13 15:44:42 +0800

    type: feat
    Add another solution to the problem Binary Tree Level Order Traversal

--- Tree/Binary Tree Level Order Traversal II/solution.py
+++ Tree/Binary Tree Level Order Traversal II/solution.py
@@ -36,3 +36,22 @@ class Solution:
         return result[::-1]
 
 # Walk through the tree level by level
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return
+        if root.left is None and root.right is None:
+            return [[root.val]]
+        self.out = []
+        self.levelOrder(root, 0)
+        return self.out[::-1]
+
+    def levelOrder(self, root, height):
+        if root is None:
+            return
+        if len(self.out) <= height:
+            self.out.append([])
+        self.out[height].append(root.val)
+        self.levelOrder(root.left, height+1)
+        self.levelOrder(root.right, height+1)
