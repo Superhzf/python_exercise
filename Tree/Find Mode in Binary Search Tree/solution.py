2e2b002d2eb1c9bc70e5c1233537eba2235341a4
Superhzf <superhzf@hotmail.com>
2020-06-18 17:54:41 +0800

    type: feat
    Add a more intuitive solution for the problem Find Mode in Binary Search 
    Tree

--- Tree/Find Mode in Binary Search Tree/solution.py
+++ Tree/Find Mode in Binary Search Tree/solution.py
@@ -29,3 +29,37 @@ class Solution(object):
 
 # The key to this solution is that the space complexity is O(1)
 # The idea is that using INORDER TRAVEL, so result would be sorted.
+
+# Solution 2
+# It is more intuitive but it cannot pass all test cases, I don't know why
+# please try yourself for more details.
+class Solution(object):
+    prev = None
+    res = []
+    curr_count = 0
+    max_count = 0
+    def findMode(self, root):
+        self.dfs(root)
+        return self.res
+
+    def dfs(self, root):
+        if root is None:
+            return None
+        if self.prev is None:
+            self.prev = root.val
+            self.curr_count = 1
+            self.max_count = 1
+        else:
+            if root.val == self.prev:
+                self.curr_count += 1
+            else:
+                self.curr_count = 1
+
+        if self.curr_count == self.max_count:
+            self.res.append(root.val)
+        elif self.curr_count > self.max_count:
+            self.res = [root.val]
+            self.max_count = self.curr_count
+        self.prev = root.val
+        self.dfs(root.left)
+        self.dfs(root.right)
